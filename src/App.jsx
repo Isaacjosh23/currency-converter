97049c5c55ed7e76d324d5d02742cbd502cf4944
Isaacjosh23 <isaacjoshua969@gmail.com>
2025-09-24 00:01:39 +0100

    Calculations done

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,32 @@ const CurrencyConverter = () => {
   const [amount, setAmount] = useState("");
   const [baseCurrency, setBaseCurrency] = useState("EUR");
   const [quoteCurrency, setQuoteCurrency] = useState("USD");
+  const [outputAmount, setOutputAmount] = useState("");
 
   // Conversion Effect
   useEffect(() => {
     async function converter() {
+      if (!amount) {
+        setOutputAmount("");
+        return;
+      }
+
       const res = await fetch(
         `https://api.frankfurter.dev/v1/latest?base=${baseCurrency}&symbols=${quoteCurrency}`
       );
 
       const data = await res.json();
 
-      const convertedAmount = (amount * data.rates[quoteCurrency]).toFixed(2);
+      const numAmount = parseFloat(amount);
+      if (isNaN(numAmount)) {
+        setOutputAmount("");
+        return;
+      }
+
+      const convertedAmount = (numAmount * data.rates[quoteCurrency]).toFixed(
+        2
+      );
+      setOutputAmount(convertedAmount);
     }
 
     converter();
@@ -37,7 +52,12 @@ const CurrencyConverter = () => {
         setQuoteCurrency={setQuoteCurrency}
       />
 
-      <CurrencyOutput />
+      <CurrencyOutput
+        amount={amount}
+        baseCurrency={baseCurrency}
+        quoteCurrency={quoteCurrency}
+        outputAmount={outputAmount}
+      />
     </div>
   );
 };
@@ -70,12 +90,14 @@ const CurrencyInput = ({
   return (
     <div className="flex flex-col gap-8 items-center">
       <input
+        inputMode="decimal"
+        pattern="[0-9]*"
         value={amount}
         onChange={(e) => {
           const value = e.target.value;
 
           if (value === "" || /^[0-9]*\.?[0-9]*$/.test(value)) {
-            setAmount(Number(value));
+            setAmount(value);
           }
         }}
         type="text"
@@ -107,8 +129,19 @@ const CurrencyInput = ({
   );
 };
 
-const CurrencyOutput = () => {
-  return <p className="text-2xl">Output</p>;
+const CurrencyOutput = ({
+  outputAmount,
+  amount,
+  baseCurrency,
+  quoteCurrency,
+}) => {
+  return (
+    <p className="text-2xl">
+      {outputAmount
+        ? `${amount} ${baseCurrency} to ${quoteCurrency} is ${outputAmount}`
+        : "How much do you want to convert?"}
+    </p>
+  );
 };
 
 export default App;
